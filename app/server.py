8b57fd8dca50846b119ea718714045f2ef27000c
mkiragu <mkiragu@gmail.com>
2024-04-25 11:18:04 +0300

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -11,23 +11,27 @@ import json
 import textract as txrct
 import tempfile
 from base64 import b64decode
-
+from io import BytesIO
+from PIL import Image
+import requests
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
-
-
 
 @app.route('/textract', methods=['POST'])
 async def textract(request):      
     data = await request.body()
     data_json = json.loads(data)
-    file_type = data_json['file_type']
-    file_dec = b64decode(data_json['data'])
+    image_url = data_json['image_url']
 
-    suffix = f'.{file_type}'
+    # Download the image from the provided URL
+    response = requests.get(image_url)
+    image = Image.open(BytesIO(response.content))
 
-    with tempfile.NamedTemporaryFile(suffix=suffix, buffering=0) as t:
-        t.write(file_dec)
+    # Convert the image to a temporary file
+    with tempfile.NamedTemporaryFile(suffix='.png', buffering=0) as t:
+        image.save(t.name)
+
+        # Extract text from the image
         text = txrct.process(t.name)
 
     resp = {'text': text.decode('utf-8')}
